dc0f4d11ef49cc1d5aa6cb40ecb2ad979ade05b3
Edward Ganly <11688686+zlodag@users.noreply.github.com>
2025-08-25 14:32:05 +0000

    bug fix - remove row factory from connection

--- wally.py
+++ wally.py
@@ -71,10 +71,10 @@ def wally_data():
     with open(file_path, 'r') as f:
         desks = {desk["ip"]:desk for desk in json.load(f)}
     with coolify_pool.connection() as conn:
-        conn.row_factory=dict_row
         with conn.execute(r"""select ris from users where sso ilike any(%s)""", [[user["username"] for desk in desks.values() for user in desk["users"]]], prepare=True) as cur:
-            ris_data = locator_ris([user["ris"] for user in cur.fetchall()])
+            ris_data = locator_ris([ris for (ris,) in cur.fetchall()])
         with conn.execute(r"""select * from users where show_in_locator and ris = any(%s)""", [[ris_user for ris_user in ris_data.keys()]], prepare=True) as cur:
+            cur.row_factory=dict_row
             users = {user["ris"]:user|ris_data[user["ris"]]|dict(windows_logons=dict()) for user in cur.fetchall()}
     sso_map = {user["sso"].lower(): uid for uid, user in users.items()}
     radiologist_desks = {}
